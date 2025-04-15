39f1dc08fac8b7ce9255cf7ad11d12a0bc4f9dec
little-planet <weiboxin100@gmail.com>
2025-04-15 23:28:45 +0900

    添加退出追击条件
    添加退出追击条件，修复追击时空指针bug

--- Scripts/FsmStateMachine/Charlie/ChairlieChaseState.cs
+++ Scripts/FsmStateMachine/Charlie/ChairlieChaseState.cs
@@ -7,23 +7,37 @@ public class ChairlieChaseState : ChairlieStateBase
 {
     private Coroutine change_speed_coroutine;
     private NavMeshAgent navMeshAgent;
+    private float memory_time;
     public override void OnStateEnter()
     {
+        Debug.Log("进入追击模式！！");
         navMeshAgent = agent.GetComponent<NavMeshAgent>();
         AudioManager.Instance.PlaySafely("战斗", E_AudioType.E_MUSIC);
         fsm.SetSpeedScaleGradiently(1f);
         fsm.StartRepeatingAction(0.2f, Pursuit);
+        memory_time = 1f;
     }
 
     public override void OnStateExit()
     {
 
         AudioManager.Instance.StopSafely("战斗");
+        navMeshAgent.ResetPath();
     }
 
     public override void OnStateUpdate()
     {
-        FindTarget();
+        if (!FindTarget())
+        {
+            memory_time -= Time.deltaTime;
+        }
+        else {
+            memory_time = 1f;
+        }
+
+        if (memory_time <= 0) {
+            fsm.ChangeToState<ChairlieIdleState>();
+        }
     }
 
     private void Pursuit() {
