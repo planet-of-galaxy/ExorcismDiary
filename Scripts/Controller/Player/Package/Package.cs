15ac6d3cfde9998abb5c6720893f826642ccdb23
little-planet <weiboxin100@gmail.com>
2025-04-29 22:26:32 +0900

    Paint预制体

---
+++ Scripts/Controller/Player/Package/Package.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+public class ClubItem {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string ImagePath { get; set; }
+
+}
+public class PropItem { }
+public class FileItem { }
+public class Package
+{
+    private List<ClubItem> ClubItems { get; set; }
+    private List<PropItem> PropItems { get; set; }
+    private List<FileItem> FileItems { get; set; }
+    public Package()
+    {
+        ClubItems = new List<ClubItem>();
+        PropItems = new List<PropItem>();
+        FileItems = new List<FileItem>();
+    }
+
+
+}
